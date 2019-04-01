84e10996457ebafaca0543160f5920a844e14de0
Dan-Baba <danfoote104227@gmail.com>
2019-04-01 11:23:46 -0400

    Added high accuracy test.

--- src/tests/UtilitiesTests.java
+++ src/tests/UtilitiesTests.java
@@ -16,4 +16,20 @@ public class UtilitiesTests {
                 System.out.println("Problem with number: " + i);
         }
     }
+
+    @Test
+    void millerRabinPrimality_correctHighProbability() {
+        for (int i = 0; i < 100000; i++) {
+            boolean actual = Utilities.millerRabinPrimality(i);
+            boolean expected = BigInteger.valueOf(i).isProbablePrime(25);
+            int runs = 0;
+            while ((actual || expected) && runs < 10) {
+                actual = Utilities.millerRabinPrimality(i) && actual;
+                expected = BigInteger.valueOf(i).isProbablePrime(25) && expected;
+                runs++;
+            }
+            if (actual != expected)
+                System.out.println("Problem with number: " + i);
+        }
+    }
 }
